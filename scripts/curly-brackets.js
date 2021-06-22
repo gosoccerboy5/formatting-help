daa041b393ee3f87fbf2bef59b4acab81c0605f4
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-06-22 15:15:11 -0500

    Make it so you can place curly brackets on newlines now
    finally

--- scripts/curly-brackets.js
+++ scripts/curly-brackets.js
@@ -2,8 +2,22 @@ const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 const button = document.querySelector("#button");
 
-button.onclick = function(event) {
-    output.value = input.value.replaceAll(/ *\n *{/g, " {");
+
+String.prototype.times = function(amt) {
+    let returnVal = "";
+    for (let i = 0; i < amt; i++) returnVal += this;
+    return returnVal;
 };
+button.onclick = function(event) {
+    if (document.querySelector("#sameline").checked) {
+        output.value = input.value.replaceAll(/ *\n *{/g, " {");
+    } else if (document.querySelector("#newline").checked) {
+        output.value = input.value.replaceAll(/^ *.*{ *$/gm, function(n) {
+            return n.substring(0, n.length - 1) + "\n" + " ".times(n.match(/^ */gi)[0].length) + "{";
+        });
+    }
+};
+document.querySelector("#sameline").addEventListener("change", button.onclick);
+document.querySelector("#newline").addEventListener("change", button.onclick);
 input.addEventListener("input", button.onclick);
 input.focus();
