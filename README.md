4423a9968c1fd6379a3e8ee55abb4d4de642eef2
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-06-25 15:24:00 -0500

    fix bug in trim-whitespace.html
     - used to handle newlines incorrectly
     - update readme.md to link to correct dartpad url,
     - recognize relatively recent support for tabs on readme.md and trim-whitespace.html

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # formatting-help
- Tools for fixing wacky formatting  
+ Tools for fixing wacky formatting. Good amount of support for tabs.  
 https://gosoccerboy5.github.io/formatting-help/
 
 ## For recreations written in Dart (cause why not):
-[trim-whitespace](https://dartpad.dev/af50de4104eb16e4d8563fc311b63992?null_safety=true)  
+[trim-whitespace](https://dartpad.dev/eb896fef2a75cf4b1bb182aed65b79ae?null_safety=true)  
 [replace-indentation](https://dartpad.dev/5d84d9ad08adbf16afc57b847e443e23?null_safety=true)  
 [curly-brackets](https://dartpad.dev/6081062503e8b6154177ff1eb7ab1d21?null_safety=true)  
 [tabs-to-spaces](https://dartpad.dev/aec0c9059054c13e64ca4cdc2a587be8?null_safety=true)  
--- scripts/trim-whitespace.js
+++ scripts/trim-whitespace.js
@@ -8,12 +8,18 @@
         // "Multiplies" a string.
     };
     trigger.onclick = function() {
-        target.value = input.value.replaceAll(
-            new RegExp("^" + "\\s".times(Number(number.value)), "gm"), 
-            ""
-        );
-        // Detect number of whitespace tokens on newlines, and replace that number of whitespace tokens
-        // With an empty string
+        let temp = ""; // We will expand this
+        for (let currentLine of input.value.split("\n")) { // iterate through each line of input
+            if (currentLine !== "") {
+                temp += currentLine.replace(
+                    new RegExp("^" + "\\s".times(Number(number.value))), ""
+                );
+                // regex matches string-start, then specified amount of whitespace
+                // Replaces it with empty string
+            }
+            temp += "\n"; // add a newline so it doesn't get smushed onto one line
+        }
+        target.value = temp;
     };
 
     input.oninput = function() {
