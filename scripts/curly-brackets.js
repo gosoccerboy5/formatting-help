b6d207e71c7059af8f786a2345354e5c3e59eb21
gosoccerboy5 <82768218+gosoccerboy5@users.noreply.github.com>
2021-07-19 12:31:16 -0500

    document code??
    i hope i didnt break anything

--- scripts/curly-brackets.js
+++ scripts/curly-brackets.js
@@ -1,18 +1,28 @@
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 const button = document.querySelector("#button");
+// Setup variables
 
 button.onclick = function(event) {
     if (document.querySelector("#sameline").checked) {
+        // We are placing curly brackets on the same line here
         output.value = input.value.replaceAll(/\s*\n\s*{/g, " {");
+        // whitespace -> newline -> whitespace -> "{", replace it with " {"
     } else if (document.querySelector("#newline").checked) {
-        output.value = input.value.replaceAll(/^.*{\s*$/gm, function(n) {
-            n = n.trimRight();
-            return n.substring(0, n.length - 1) + "\n" + n.match(/^\s?/)[0].repeat(n.match(/^\s*/gi)[0].length) + "{";
-        });
+        // We are placing curly brackets on a new line here
+        output.value = input.value.replaceAll(
+            /^.*{\s*$/gm, // line/string-start -> any characters -> "{" -> whitespace -> end of string/line
+            function(n) {
+                n = n.trimRight(); // Remove any unexpected whitespace at the end
+                return n.substring(0, n.length - 1) + "\n" + n.match(/^\s?/)[0].repeat(n.match(/^\s*/gi)[0].length) + "{";
+                // No idea what the above line is :P
+            }
+        );
     }
 };
+
 document.querySelector("#sameline").addEventListener("change", button.onclick);
 document.querySelector("#newline").addEventListener("change", button.onclick);
 input.addEventListener("input", button.onclick);
 input.focus();
+// Add event listeners to other elements
--- scripts/tabs-to-spaces.js
+++ scripts/tabs-to-spaces.js
@@ -3,19 +3,25 @@ const output = document.querySelector("#output");
 const button = document.querySelector("#button");
 const radio = document.querySelector("#tabs2spaces");
 const spacesPerTabs = document.querySelector("#spacesPerTabs");
+// Setup variables
 
 spacesPerTabs.value = "4";
 input.focus();
+// Setup page
 
 input.oninput = function(event) {
     if (radio.checked) {
+        // We are converting tabs to spaces
         output.value = input.value.replaceAll(/^\t*/gm, function(n) {
             return " ".repeat(n.length * Number(spacesPerTabs.value));
+            // Match tabs after newline, then replace it with spaces
         });
     } else {
+        // Spaces to tabs
         output.value = input.value.replaceAll(
             /^ */gm,
             n => "\t".repeat(n.length / Number(spacesPerTabs.value))
+            // Match spaces after newline, then replace it with tabs
         );
     }
 };
@@ -24,3 +30,4 @@ button.onclick = input.oninput;
 radio.onchange = input.oninput;
 document.querySelector("#spaces2tabs").onchange = input.oninput;
 spacesPerTabs.oninput = input.oninput;
+// Add event listeners for onchange/oninput for other input methods
